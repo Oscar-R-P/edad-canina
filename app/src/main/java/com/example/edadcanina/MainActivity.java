8bd93a0aa9c017f13ac3e1e1d898c542d4970fe5
Oscar Ruiz Paez <oscar.ruiz@integralvending.com>
2024-09-18 20:12:07 -0600

    feat/inicio: Se agrega funcionalidad para calcular años perros

--- app/src/main/java/com/example/edadcanina/MainActivity.java
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EditText editText = findViewById(R.id.txtEditEdad);
+        TextView textView = findViewById(R.id.resultadoViewId);
+        Button button = findViewById(R.id.btnCalcular);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String edad = editText.getText().toString();
+                int edadInt = Integer.parseInt(edad);
+                int res = edadInt * 7;
+                String resStr = "La edad de tu perro es: "+ res+" años";
+                textView.setText(resStr);
+            }
         });
     }
 }
